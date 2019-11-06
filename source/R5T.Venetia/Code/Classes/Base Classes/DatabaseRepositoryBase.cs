cad01444a8e95076b36ebbe120ef3cd48e2e52f7
MinexAutomation <minex.automation@gmail.com>
2019-11-05 18:48:06 -0800

    Added a repository base class for repositories that manage their own DbContexts.

---
+++ source/R5T.Venetia/Code/Classes/Base Classes/DatabaseRepositoryBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace R5T.Venetia
+{
+    /// <summary>
+    /// Base class for repositories that can create their own database context instance from DI-container provided <see cref="DbContextOptions{TContext}"/>.
+    /// </summary>
+    public abstract class DatabaseRepositoryBase<TDbContext>
+        where TDbContext : DbContext
+    {
+        protected DbContextOptions<TDbContext> DbContextOptions { get; }
+
+
+        public DatabaseRepositoryBase(DbContextOptions<TDbContext> dbContextOptions)
+        {
+            this.DbContextOptions = dbContextOptions;
+        }
+
+        /// <summary>
+        /// Basically just a wrapper for callings the <typeparamref name="TDbContext"/> constructor that takes the <see cref="DbContextOptions{TContext}"/>.
+        /// </summary>
+        public abstract TDbContext GetNewDbContext();
+    }
+}
