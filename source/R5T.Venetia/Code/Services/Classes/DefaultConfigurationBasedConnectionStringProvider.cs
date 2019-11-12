1e9d49283a6e9a9d5f4d242448f1a44f0341b437
MinexAutomation <minex.automation@gmail.com>
2019-11-11 20:22:19 -0800

    Added a default configuration-based connection string provider.

---
+++ source/R5T.Venetia/Code/Services/Classes/DefaultConfigurationBasedConnectionStringProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+
+
+namespace R5T.Venetia
+{
+    /// <summary>
+    /// A default implementation of a configuration-based connection string provider service.
+    /// The service directly provides the value in the configuration at the <see cref="DefaultConfigurationBasedConnectionStringProvider.ConnectionStringConfigurationPath"/> location.
+    /// </summary>
+    public class DefaultConfigurationBasedConnectionStringProvider : IConnectionStringProvider
+    {
+        public const string ConnectionStringConfigurationPath = "DatabaseConfiguration:ConnectionString";
+
+
+        private IConfiguration Configuration { get; }
+
+
+        public DefaultConfigurationBasedConnectionStringProvider(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public string GetConnectionString()
+        {
+            var connectionString = this.Configuration[DefaultConfigurationBasedConnectionStringProvider.ConnectionStringConfigurationPath];
+            return connectionString;
+        }
+    }
+}
