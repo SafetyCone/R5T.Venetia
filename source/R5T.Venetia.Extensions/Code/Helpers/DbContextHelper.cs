6622317bcefd3bf7c091aa20f74f3bca4e7a6b28
David Coats <david@getsafetycone.com>
2021-05-27 16:24:57 -0700

    Add new R5T.Venetia.Extensions library (from deprecated R5T.Venetia.Extensions/R5T.Venetia.Extensions).

---
+++ source/R5T.Venetia.Extensions/Code/Helpers/DbContextHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace Microsoft.EntityFrameworkCore
+{
+    public static class DbContextHelper
+    {
+        public static Func<DbContextOptions<TDbContext>, Task<TDbContext>> GetAsynchronousConstructor<TDbContext>(
+            Func<DbContextOptions<TDbContext>, TDbContext> synchronousConstructor)
+            where TDbContext : DbContext
+        {
+            Task<TDbContext> Temp(DbContextOptions<TDbContext> dbContextOptions)
+            {
+                var dbContext = synchronousConstructor(dbContextOptions);
+
+                return Task.FromResult(dbContext);
+            }
+
+            return Temp;
+        }
+    }
+}
